fa7387157644bff8f0136c775fbadf6f24f32ea8
Leonardo Silva <leonardosilvaif@hotmail.com>
2023-11-05 11:03:02 -0300

    feat: first commit

---
+++ src/main/java/com/demo/security/jwt/service/UserService.java
@@ -0,0 +1,21 @@
+package com.demo.security.jwt.service;
+
+import com.demo.security.jwt.model.User;
+import com.demo.security.jwt.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private PasswordEncoder encoder;
+    public void createUser(User user){
+        String pass = user.getPassword();
+        //criptografando antes de salvar no banco
+        user.setPassword(encoder.encode(pass));
+        repository.save(user);
+    }
+}
---
+++ src/test/java/com/demo/security/jwt/DemoSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
